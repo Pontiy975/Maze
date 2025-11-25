c7160b7df51d5c50b56bc63922d1056362533426
Anrii Kuriachyi <a.kuryachiy975@gmail.com>
2025-11-25 23:11:08 +0400

    initial commit

---
+++ Assets/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+public static class FloatExtensions
+{
+    public static float GetSqr(this float value) => value * value;
+}
---
+++ Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,65 @@
+using DG.Tweening;
+using System;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static float GetSqrDistance(this Transform origin, Transform target)
+    {
+        return (target.position - origin.position).sqrMagnitude;
+    }
+
+    public static float GetSqrDistance(this Transform origin, Vector3 target)
+    {
+        return (target - origin.position).sqrMagnitude;
+    }
+    
+    public static bool IsSameTransform(this Transform origin, Transform target)
+    {
+        return origin == target;
+    }
+
+    public static Vector3 GetDirectionToPoint(this Transform origin, Transform target, Space space = Space.World)
+    {
+        return space == Space.World ? (target.position - origin.position).normalized : (target.localPosition - origin.localPosition).normalized;
+    }
+
+    public static Rect GetWorldRect(this RectTransform transform)
+    {
+        Vector3[] corners = new Vector3[4];
+        transform.GetWorldCorners(corners);
+
+        return new(corners[0].x, corners[0].y,
+                   corners[2].x - corners[0].x,
+                   corners[2].y - corners[0].y);
+    }
+
+    public static void Bounce(this Transform transform, Vector3 baseScale, Vector3 targetScale, float duration = 0.2f, int loops = 0, Action onComplete = null)
+    {
+        transform.DOKill();
+        transform.localScale = baseScale;
+
+        transform.DOScale(targetScale, duration)
+                 .SetEase(Ease.OutSine)
+                 .SetLoops(loops, LoopType.Yoyo)
+                 .OnComplete(() =>
+                 {
+                     if (loops == 0)
+                     {
+                         transform.DOScale(baseScale, duration / 2f)
+                                  .SetEase(Ease.InSine)
+                                  .OnComplete(() => onComplete?.Invoke());
+                     }
+                     else
+                     {
+                         onComplete?.Invoke();
+                     }
+                 });
+    }
+
+    public static void ResetBounce(this Transform transform, Vector3 baseScale)
+    {
+        transform.DOKill();
+        transform.localScale = baseScale;
+    }
+}
